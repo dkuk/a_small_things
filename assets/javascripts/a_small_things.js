df6cd1bbf8773af1774f67ff5841c39b2d04e307
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2012-12-03 15:44:48 +1100

    Issue #5074. Js changed to jquery

--- assets/javascripts/a_small_things.js
+++ assets/javascripts/a_small_things.js
@@ -1,61 +1,55 @@
-document.observe('dom:loaded', function(){
-	$$('a.icon').each(function(e){
-		e.update('<span>'+e.innerHTML+'</span>');
-		});
-	$$('tr.hascontextmenu').each(function(e){
-		e.removeClassName('hascontextmenu');
-	});
+$(document).ready(function(){
+  /*jQuery('input.issue_estimated_hours, input.estimated_hours').live('input', function(){
+      calcDueDate();  
+    });*/
+  $('input.issue_due_date').live('click', function(){
+      $(this).data('set_auto_val', false);
+      $(this).removeClass('highlight');
+    });
 
+  $('td.project>a').each(function(index){
+    $(this).parent().html($(this).html());
+  });
 
-	});
+  $('a.icon').each(function(index){
+    $(this).html('<span>'+$(this).html()+'</span>');
+  });
+  
+  $('tr.hascontextmenu').removeClass('hascontextmenu');
 
-jQuery(document).ready(function(){
-	/*jQuery('input.issue_estimated_hours, input.estimated_hours').live('input', function(){
-			calcDueDate();	
-		});*/
-	jQuery('input.issue_due_date').live('click', function(){
-			jQuery(this).data('set_auto_val', false);
-			jQuery(this).removeClass('highlight');
-		});
-
-	jQuery('td.project>a').each(function(index){
-		jQuery(this).parent().html(jQuery(this).html());
-	});
-	
-
-	});
+});
 
 function checkDueDateCalcIsHandy()
-	{
-		var start_date=jQuery('input.issue_start_date');
-		var due_date=jQuery('input.issue_due_date');
-		var estimated_hours=jQuery('input.issue_estimated_hours');
-		if(start_date && estimated_hours && due_date )
-			{
-			if(due_date.val()=='' || (due_date.data('set_auto_val') ) )
-				{
-				if(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(start_date.val()) && /^[0-9\.]{1,}$/.test(estimated_hours.val()))
-	                calcDueDate(start_date, due_date, estimated_hours);
-				}
-			else
-				{
-				if(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(start_date.val())
-					&& /^[0-9\.]{1,}$/.test(estimated_hours.val())
-					&& /^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(due_date.val()))
-					{
-					var date_arr=start_date.val().split(".");
-					var issue_start_date = new Date(Date.parse(date_arr[2]+"-"+date_arr[1]+"-"+date_arr[0]));		
-					date_arr=due_date.val().split(".");	
-					var issue_due_date = new Date(Date.parse(date_arr[2]+"-"+date_arr[1]+"-"+date_arr[0]));	
-					var days=(estimated_hours.val()/8).ceil();
-					if(issue_due_date.getDate()-issue_start_date.getDate()<days-1)
-					    calcDueDate(start_date, due_date, estimated_hours);
-					}
+  {
+    var start_date=jQuery('input.issue_start_date');
+    var due_date=jQuery('input.issue_due_date');
+    var estimated_hours=jQuery('input.issue_estimated_hours');
+    if(start_date && estimated_hours && due_date )
+      {
+      if(due_date.val()=='' || (due_date.data('set_auto_val') ) )
+        {
+        if(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(start_date.val()) && /^[0-9\.]{1,}$/.test(estimated_hours.val()))
+                  calcDueDate(start_date, due_date, estimated_hours);
+        }
+      else
+        {
+        if(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(start_date.val())
+          && /^[0-9\.]{1,}$/.test(estimated_hours.val())
+          && /^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/.test(due_date.val()))
+          {
+          var date_arr=start_date.val().split(".");
+          var issue_start_date = new Date(Date.parse(date_arr[2]+"-"+date_arr[1]+"-"+date_arr[0]));   
+          date_arr=due_date.val().split("."); 
+          var issue_due_date = new Date(Date.parse(date_arr[2]+"-"+date_arr[1]+"-"+date_arr[0])); 
+          var days=(estimated_hours.val()/8).ceil();
+          if(issue_due_date.getDate()-issue_start_date.getDate()<days-1)
+              calcDueDate(start_date, due_date, estimated_hours);
+          }
 
-				}
-			}
+        }
+      }
 
-	}
+  }
 
 function calcDueDate(start_date, due_date, estimated_hours)
     {
@@ -66,7 +60,7 @@ function calcDueDate(start_date, due_date, estimated_hours)
     var days=(estimated_hours.val()/8).ceil();
     var issue_due_date = new Date(issue_start_date.getTime()+(days-1)*1000*24*60*60+1);
     //issue_due_date.setDate(issue_start_date.getDate()+days-1);
-				
+        
     var month= issue_due_date.getMonth()+1;
     month=(month<10)? '0'+month : month;
     var day=issue_due_date.getDate();
@@ -78,5 +72,5 @@ function calcDueDate(start_date, due_date, estimated_hours)
     }
 
 setInterval(function() { 
-		checkDueDateCalcIsHandy();
- 	}, 1000)
+    checkDueDateCalcIsHandy();
+  }, 1000)
